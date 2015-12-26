a17c7f3dd7a36bf9dd0942cfcc718d66efd92199
daniellescheide <ditisDanielle@users.noreply.github.com>
2015-12-26 18:40:21 +0100

    DatabaseAccess.properties aangepast: bevatte fouten. Librarie JDK 1.7 omgezet naar JDK 1.8. Librarie MySQL JDBC Driver toegevoegd. Frame FulHouse.java toegevoegd met op tabblad Speler een button die een nieuw scherm (spelersOverzicht.java) met een jTable opent. Dit principe kunnen we gebruiken voor alle gevraagde overzichten. ModelItem.java toegevoegd maar nog niet gebruikt in code.

---
+++ the-poker-database-challenge/src/ModelItem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author gamijnar
+ */
+public class ModelItem {
+
+    public int id;
+    public String naam;
+    public String adres;
+    public String woonplaats;
+
+    @Override
+    public String toString() {
+              
+      return naam;
+    }
+
+}
--- the-poker-database-challenge/src/Toernooi.java
+++ the-poker-database-challenge/src/Toernooi.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JFrame;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,12 +17,10 @@ public class Toernooi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello pokerworld!");
-        System.out.println("Danielle is in the house");
-        System.out.println("Stefan is also in the ");
-        System.out.println("Bla bla, bla");
-        System.out.println("Hoi!");
-        System.out.println("Over een half uurtje wil ik aftaaien zegt Danielle");
+        JFrame frame = new FullHouse();
+        //centreer het frame:
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
     
 }
